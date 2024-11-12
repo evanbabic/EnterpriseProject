d801c7427c08f0504dab7f5986074ed10618c9cd
babice <babice@sheridancollege.ca>
2024-11-11 19:18:20 -0500

    Idk

--- EnterpriseProject/obj/Debug/net8.0/EnterpriseProject.AssemblyInfo.cs
+++ EnterpriseProject/obj/Debug/net8.0/EnterpriseProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnterpriseProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd8129dc7e47466b38f34e47e807ace926ed5a91")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnterpriseProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnterpriseProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
