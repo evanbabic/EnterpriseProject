5d66789bdf6778b64eaa0c95f0430b9e63703f67
Nicholas Williams <nickwill820@gmail.com>
2024-11-30 19:11:46 -0500

    Fixed layout
    - Fixed layout header buttons

--- EnterpriseProject/Controllers/ProjectController.cs
+++ EnterpriseProject/Controllers/ProjectController.cs
@@ -1,4 +1,5 @@
-﻿using EnterpriseProject.Services.Repositories;
+﻿using EnterpriseProject.Entities;
+using EnterpriseProject.Services.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EnterpriseProject.Operations.Controllers
