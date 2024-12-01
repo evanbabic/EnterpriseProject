836860cd2a25e833e61f88db12f93bafad918d46
Nicholas Williams <nickwill820@gmail.com>
2024-11-30 22:06:42 -0500

    Reworked resume storage
    - Made resume file storage follow common practices

--- EnterpriseProject/Controllers/ResumeController.cs
+++ EnterpriseProject/Controllers/ResumeController.cs
@@ -7,15 +7,20 @@ using System.Security.Claims;
 namespace EnterpriseProject.Operations.Controllers
 {
 	[Authorize]
-	public class ResumeController(IResumeRepository resumeRepository, IWebHostEnvironment webHostEnvironment) : Controller
+	public class ResumeController(IResumeRepository resumeRepository) : Controller
 	{
+		private static readonly HashSet<string> resumeFileExtensions = [".pdf"];
+		private static readonly string resumeFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", "resumes");
+		private static readonly string relativeResumeFolderPath = "/uploads/resumes";
+
 		private readonly IResumeRepository resumeRepository = resumeRepository;
-		private readonly IWebHostEnvironment webHostEnvironment = webHostEnvironment;
 
 		[HttpGet]
 		public IActionResult ViewResume(int resumeId)
 		{
 			Resume? resume = resumeRepository.GetResume(resumeId);
+
+			if (resume != null) ViewBag.ResumeFilePath = Path.Combine(relativeResumeFolderPath, resume.FilePath);
 
 			Claim? myIdClaim = User.FindFirst(Entities.User.ClaimType);
 			int? myId = myIdClaim != null ? int.Parse(myIdClaim.Value) : null;
@@ -33,7 +38,7 @@ namespace EnterpriseProject.Operations.Controllers
 				return RedirectToAction(nameof(UserController.Profile), nameof(Entities.User), new { userId });
 			}
 
-			string userResumePath = Path.Combine(webHostEnvironment.WebRootPath, Resume.Directory, userId.ToString(), userResume.FilePath);
+			string userResumePath = Path.Combine(resumeFolderPath, userResume.FilePath);
 			System.IO.File.Delete(userResumePath);
 
 			resumeRepository.DeleteResume(userResume.ResumeId);
@@ -44,44 +49,46 @@ namespace EnterpriseProject.Operations.Controllers
 		[HttpPost]
 		public async Task<IActionResult> UploadResume(int userId, IFormFile resumeFile)
 		{
-			if (resumeFile == null || resumeFile.Length == 0 || !Path.GetExtension(resumeFile.FileName).Equals(Resume.FileExtension, StringComparison.CurrentCultureIgnoreCase))
+			if (resumeFile == null || resumeFile.Length == 0 || !HasValidExtension(resumeFile))
 			{
 				return RedirectToAction(nameof(UserController.Profile), nameof(Entities.User), new { id = userId });
 			}
 
-			string resumeDirectory = Path.Combine(webHostEnvironment.WebRootPath, Resume.Directory);
-			if (!Directory.Exists(resumeDirectory)) Directory.CreateDirectory(resumeDirectory);
-			string userResumeDirectory = Path.Combine(resumeDirectory, userId.ToString());
-			if (!Directory.Exists(userResumeDirectory)) Directory.CreateDirectory(userResumeDirectory);
+			_ = Directory.CreateDirectory(resumeFolderPath);
 
-			using (FileStream fileStream = new(Path.Combine(userResumeDirectory, resumeFile.FileName), FileMode.Create))
-			{
-				await resumeFile.CopyToAsync(fileStream);
-			}
+			string fileName = Path.GetFileNameWithoutExtension(resumeFile.FileName) + "_" + Guid.NewGuid().ToString() + Path.GetExtension(resumeFile.FileName);
 
 			Resume? userResume = resumeRepository.GetResumes().FirstOrDefault(resume => resume.UserId == userId);
 			if (userResume != null)
 			{
-				if (userResume.FilePath != resumeFile.FileName)
-				{
-					string oldFilePath = Path.Combine(userResumeDirectory, userResume.FilePath);
-					System.IO.File.Delete(oldFilePath);
+				string oldFilePath = Path.Combine(resumeFolderPath, userResume.FilePath);
+				System.IO.File.Delete(oldFilePath);
 
-					userResume.FilePath = resumeFile.FileName;
-					resumeRepository.UpdateResume(userResume);
-				}
+				userResume.FilePath = fileName;
+				resumeRepository.UpdateResume(userResume);
 			}
 			else
 			{
 				userResume = new Resume
 				{
-					FilePath = resumeFile.FileName,
+					FilePath = fileName,
 					UserId = userId
 				};
 				resumeRepository.AddResume(userResume);
 			}
 
+			using (FileStream fileStream = new(Path.Combine(resumeFolderPath, fileName), FileMode.Create))
+			{
+				await resumeFile.CopyToAsync(fileStream);
+			}
+
 			return RedirectToAction(nameof(ViewResume), new { resumeId = userResume.ResumeId });
+		}
+
+		private static bool HasValidExtension(IFormFile resumeFile)
+		{
+			string resumeFileExtension = Path.GetExtension(resumeFile.FileName);
+			return resumeFileExtensions.Any(extension => extension.Equals(resumeFileExtension, StringComparison.CurrentCultureIgnoreCase));
 		}
 	}
 }
